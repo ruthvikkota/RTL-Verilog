8c44a2318422e7def4c0eaeb1104b21c5f3a2885
Ruthvik Kota <88280622+ruthvikkota@users.noreply.github.com>
2023-12-12 16:49:45 +0530

    Update full_adder_gate_level.v

--- 02_full_adder/full_adder_gate_level.v
+++ 02_full_adder/full_adder_gate_level.v
@@ -7,12 +7,11 @@ ha h2(.a(w1),.b(cin),.sum(sum),.carry(w3));
 or(cout,w3,w2);
 endmodule
 
-/*
+/* half adder verilog code
 module ha(a,b,sum,carry);
 input a,b;
 output sum,carry;
 xor(sum,a,b);
 and(carry,a,b);
 endmodule
-
 */
