3d537aed029f64b2269ec8e9c52d951d781bc4a1
Ruthvik Kota <88280622+ruthvikkota@users.noreply.github.com>
2023-12-12 16:48:43 +0530

    Update and rename half_adder_gate_level.v.txt to full_adder_gate_level.v

---
+++ 02_full_adder/full_adder_gate_level.v
@@ -0,0 +1,18 @@
+module fa(a,b,cin,sum,cout);
+input a,b,cin;
+output sum,cout;
+wire w1,w2,w3;
+ha h1(.a(a),.b(b),.sum(w1),.carry(w2));
+ha h2(.a(w1),.b(cin),.sum(sum),.carry(w3));
+or(cout,w3,w2);
+endmodule
+
+/*
+module ha(a,b,sum,carry);
+input a,b;
+output sum,carry;
+xor(sum,a,b);
+and(carry,a,b);
+endmodule
+
+*/
